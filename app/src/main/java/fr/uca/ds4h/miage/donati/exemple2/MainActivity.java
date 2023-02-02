71484e61f3d51bbfdb7f622b86eea5cd55afed41
Leo Donati <leo.donati@unice.fr>
2023-02-02 09:13:54 +0100

    Changé le gradlepour ne pas avoir de conflit avec l'exemple 1

--- app/src/main/java/fr/uca/ds4h/miage/donati/exemple2/MainActivity.java
+++ app/src/main/java/fr/uca/ds4h/miage/donati/exemple2/MainActivity.java
@@ -1,10 +1,9 @@
-package fr.uca.ds4h.miage.donati.exemple1;
+package fr.uca.ds4h.miage.donati.exemple2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
